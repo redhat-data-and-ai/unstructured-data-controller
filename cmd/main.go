0a69bb407affd3d8ec8bc6f5e918d65dd31addcc
shikgupt <shikgupt@redhat.com>
2026-02-10 18:29:52 +0530

    DATA-5830 - Add Dockerfile, MakeFile

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -178,6 +179,16 @@ func main() {
 		})
 	}
 
+	// Get the namespace to watch. If not set, defaults to all namespaces.
+	watchNamespace := os.Getenv("WATCH_NAMESPACE")
+	var cacheDefaultNamespaces map[string]cache.Config
+	if watchNamespace != "" {
+		setupLog.Info("Watching resources only in namespace", "namespace", watchNamespace)
+		cacheDefaultNamespaces = map[string]cache.Config{
+			watchNamespace: {},
+		}
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
@@ -185,6 +196,9 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "6dc1af3d.dataverse.redhat.com",
+		Cache: cache.Options{
+			DefaultNamespaces: cacheDefaultNamespaces,
+		},
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
